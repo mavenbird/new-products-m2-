6c04308aed7a9f03bb9ec70e0ef1b3fa7febe279
Harshil Patwa <harshil@mavenbird.com>
2024-09-02 12:29:25 +0000

    add comments

--- Block/Adminhtml/Product/Grid.php
+++ Block/Adminhtml/Product/Grid.php
@@ -1,10 +1,22 @@
 <?php
-/***************************************************************************
- Extension Name    : New Products
- Extension URL    : http://www.mavenbird.com/new-products-extension-for-magento-2.html
- Copyright        : Copyright (c) 2016 MavenBird, http://www.mavenbird.com
- Support Email    : support@mavenbird.com
- ***************************************************************************/
+/**
+* Mavenbird Technologies Private Limited
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the EULA
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://mavenbird.com/Mavenbird-Module-License.txt
+*
+* =================================================================
+*
+* @category   Mavenbird
+* @package    Mavenbird_Newproduct
+* @author     Mavenbird Team
+* @copyright  Copyright (c) 2018-2024 Mavenbird Technologies Private Limited ( http://mavenbird.com )
+* @license    http://mavenbird.com/Mavenbird-Module-License.txt
+*/
 namespace  Mavenbird\Newproduct\Block\Adminhtml\Product;
 
 use Magento\Store\Model\Store;
